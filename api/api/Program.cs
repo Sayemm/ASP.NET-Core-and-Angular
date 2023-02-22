43e15698ce6bbfed2e15f376c74f02d5848a6e84
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-22 14:52:44 +0600

    Refactoed program.cs file

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -3,23 +3,15 @@ using api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddCors(options =>
-{
-    var frontendURL = builder.Configuration.GetValue<string>("frontend_url");
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins(frontendURL!).AllowAnyMethod().AllowAnyHeader();
-    });
-});
+builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddIdentityServices(builder.Configuration);
 
 
@@ -29,13 +21,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseCors();
+app.UseCors(x => x.AllowAnyHeader()
+  .AllowAnyMethod()
+  .AllowCredentials()
+  .WithOrigins(builder.Configuration.GetValue<string>("frontend_url")!));
+
+
+
 app.UseAuthentication();
 app.UseAuthorization();
 
