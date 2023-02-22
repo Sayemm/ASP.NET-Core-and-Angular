dce7134d47708fc9d745268dbe081e509d583ebc
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-22 11:54:03 +0600

    Added new password column for user

--- api/api/Entities/AppUser.cs
+++ api/api/Entities/AppUser.cs
@@ -1,8 +1,10 @@
 ﻿namespace api.Entities
 {
-    public class AppUser
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-    }
+  public class AppUser
+  {
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public byte[] PaswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+  }
 }
