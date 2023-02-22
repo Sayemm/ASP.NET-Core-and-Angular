e409824a8d32ce11edc1ac9dc042d3c9ebb158d1
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-22 13:28:29 +0600

    Added login condition

--- api/api/DTOs/RegisterDto.cs
+++ api/api/DTOs/RegisterDto.cs
@@ -1,8 +1,13 @@
-﻿namespace api.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.DTOs
 {
   public class RegisterDto
   {
+    [Required]
     public string? UserName { get; set; }
+
+    [Required]
     public string? Password { get; set; }
   }
 }
