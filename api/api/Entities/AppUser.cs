1c932ab461782048731e777246c4a717876ffb8c
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-22 12:49:30 +0600

    made field nullable

--- api/api/Entities/AppUser.cs
+++ api/api/Entities/AppUser.cs
@@ -3,8 +3,8 @@
   public class AppUser
   {
     public int Id { get; set; }
-    public string UserName { get; set; }
-    public byte[] PaswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
+    public string? UserName { get; set; }
+    public byte[]? PaswordHash { get; set; }
+    public byte[]? PasswordSalt { get; set; }
   }
 }
