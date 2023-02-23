3ee268f4f3543c597c82aa514c38bcdbf0deb75a
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-23 12:42:40 +0600

    home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
-import { TestComponent } from './components/test/test.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    TestComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
