0c3251bd73d93ed874df016c5656c93c80f79f01
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-23 15:27:25 +0600

    register user

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,26 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   registerMode = false;
-  users: any;
 
-  constructor(private http: HttpClient) {
-    this.users = this.getUsers();
+  constructor() {
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:7211/api/users')
-      .subscribe({
-        next: (result) => {
-          this.users = result;
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      })
   }
 
   cancelRegisterMode(event: boolean) {
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,12 +7,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
+  constructor(private accountService: AccountService) {
+  }
+
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: er => {
+        console.log(er);
+      }
+    })
   }
 
   cancel() {
