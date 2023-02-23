217d0005d7a0ea600e359356d0a807ea67448b8e
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-02-23 15:07:03 +0600

    @Input() - parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,33 +10,18 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit{
   title = 'The Dating App';
-  users: any;
 
   constructor(
-    private http: HttpClient,
     private accountService: AccountService
   ) {
   }
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:7211/api/users')
-      .subscribe({
-        next: (result) => {
-          this.users = result;
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      })
   }
 }
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,8 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   registerMode = false;
+  users: any;
+
+  constructor(private http: HttpClient) {
+    this.users = this.getUsers();
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:7211/api/users')
+      .subscribe({
+        next: (result) => {
+          this.users = result;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
   }
 }
