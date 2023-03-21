9c97dcedff20a19c5442c60ec90b2911e36ee5b6
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-03-21 23:30:34 +0600

    update user profile in the client end

--- client/src/app/components/members/member-edit/member-edit.component.ts
+++ client/src/app/components/members/member-edit/member-edit.component.ts
@@ -16,7 +16,8 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
-  @HostListener('window:beforeunload', ['$event']) unloadNotification( // access to browser event
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    // access to browser event
     $event: any
   ) {
     if (this.editForm.dirty) {
@@ -49,8 +50,9 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated properly');
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated properly');
+      this.editForm.reset(this.member);
+    });
   }
 }
