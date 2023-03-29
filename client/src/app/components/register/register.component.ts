fdb8a51c5ca95a30c114b4445b235c381a474874
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2023-03-29 22:48:57 +0600

    reactive form - custom validator

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from 'src/app/_services/account.service';
 
@@ -30,8 +36,19 @@ export class RegisterComponent implements OnInit {
         Validators.minLength(4),
         Validators.maxLength(8),
       ]),
-      confirmPassword: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password'),
+      ]),
     });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value
+        ? null
+        : { isMatching: true };
+    };
   }
 
   register() {
